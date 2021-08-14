e6d120be87d8ebe1e9f11506de0e2aab18740419
Mohammed-Alramahi <vittosc1997@gmail.com>
2021-08-15 01:49:19 +0300

    fixes

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { print } from 'pretty-print';
+import './result.css'
 function Results(props) {
-
   return (
-
     <section section >
       <pre data-testid="results">{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
     </section >
